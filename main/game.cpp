a499edebe2f4c4e121da518dc5831d582836e67c
佐々木奏一郎 <241307@st.yoshida-g.ac.jp>
2025-01-28 10:47:51 +0900

    h

--- main/game.cpp
+++ main/game.cpp
@@ -47,12 +47,9 @@ void InitGame(void)
 	InitEnemy();
 	InitEdit();
 	LoadEdit();
-<<<<<<< HEAD
 	InitBlock();
 	Setitem(D3DXVECTOR3(50.0f, 40.0f, 0.0f), ITEMTYPE_ONE);
-=======
 	Setitem(D3DXVECTOR3(50.0f, 40.0f, 0.0f), ITEMTYPE_FOUR);
->>>>>>> remotes/origin/main
 
 	InitBillboard();
 	SetBillboard(D3DXVECTOR3(-100.0f, 50.0f, -200.0f), D3DXVECTOR3(0.0f, 0.0f, 0.0f), BILLBOARDTYPE_0,D3DXVECTOR3(15.0f,35.0f,0.0f));
